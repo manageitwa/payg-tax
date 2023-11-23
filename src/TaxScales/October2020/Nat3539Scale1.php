11aeaa2d3f1582d3a647664502800000931fac23
Ben Thomson <git@alfreido.com>
2023-11-23 15:56:02 +0800

    Add STSL tax scales and tests

---
+++ src/TaxScales/October2020/Nat3539Scale1.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace ManageIt\PaygTax\TaxScales\October2020;
+
+use ManageIt\PaygTax\Entities\Earning;
+use ManageIt\PaygTax\Entities\Payee;
+use ManageIt\PaygTax\Entities\Payer;
+use ManageIt\PaygTax\TaxScales\BaseCoefficientScale;
+use ManageIt\PaygTax\Utilities\Date;
+
+/**
+ * Tax scale for Australian residents not claiming the tax free threshold and have an STSL debt.
+ *
+ * This tax scale has come into effect from 13 October 2020.
+ */
+class Nat3539Scale1 extends BaseCoefficientScale
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected array $coefficients = [
+        88 => [0.1900, 0.1900],
+        371 => [0.2348, 3.9639],
+        515 => [0.2190, -1.9003],
+        546 => [0.3477, 64.4297],
+        685 => [0.3577, 64.4297],
+        747 => [0.3677, 64.4297],
+        813 => [0.3727, 64.4297],
+        882 => [0.3777, 64.4297],
+        932 => [0.3827, 64.4297],
+        956 => [0.3800, 61.9132],
+        1035 => [0.3850, 61.9132],
+        1118 => [0.3900, 61.9132],
+        1206 => [0.3950, 61.9132],
+        1299 => [0.4000, 61.9132],
+        1398 => [0.4050, 61.9132],
+        1503 => [0.4100, 61.9132],
+        1615 => [0.4150, 61.9132],
+        1732 => [0.4200, 61.9132],
+        1855 => [0.4250, 61.9132],
+        1957 => [0.4300, 61.9132],
+        1990 => [0.4750, 150.0093],
+        2130 => [0.4800, 150.0093],
+        2279 => [0.4850, 150.0093],
+        3111 => [0.4900, 150.0093],
+        999999999 => [0.57, 398.9324],
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isEligible(Payer $payer, Payee $payee, Earning $earning): bool
+    {
+        // Only applies after 13 October 2020.
+        if (!Date::from($earning->getPayDate(), '2020-10-13')) {
+            return false;
+        }
+
+        // Only applies to Australian residents.
+        if ($payee->getResidencyStatus() !== \ManageIt\PaygTax\Entities\Payee::RESIDENT) {
+            return false;
+        }
+
+        // Only applies to payees with a tax file number.
+        if (!$payee->hasTfnNumber()) {
+            return false;
+        }
+
+        // Only applies to payees not claiming the tax free threshold.
+        if ($payee->claimsTaxFreeThreshold()) {
+            return false;
+        }
+
+        // Only applies to payees with an STSL debt.
+        if (!$payee->hasSTSLDebt()) {
+            return false;
+        }
+
+        // Only applies to payees with no Medicare Levy exemption.
+        if ($payee->getMedicareLevyExemption() !== \ManageIt\PaygTax\Entities\Payee::MEDICARE_LEVY_EXEMPTION_NONE) {
+            return false;
+        }
+
+        // Only applies to payees not claiming the Seniors Offset.
+        if ($payee->getSeniorsOffset() !== \ManageIt\PaygTax\Entities\Payee::SENIORS_OFFSET_NONE) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/TaxScales/October2020/Nat3539Scale5.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace ManageIt\PaygTax\TaxScales\October2020;
+
+use ManageIt\PaygTax\Entities\Earning;
+use ManageIt\PaygTax\Entities\Payee;
+use ManageIt\PaygTax\Entities\Payer;
+use ManageIt\PaygTax\TaxScales\BaseCoefficientScale;
+use ManageIt\PaygTax\Utilities\Date;
+
+/**
+ * Tax scale for Australian residents who are claiming the tax free threshold and are claiming a full
+ * Medicare levy exemption and have an STSL debt.
+ *
+ * This tax scale has come into effect from 13 October 2020.
+ */
+class Nat3539Scale5 extends BaseCoefficientScale
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected array $coefficients = [
+        359 => [0.0000, 0.0000],
+        721 => [0.1900, 68.3462],
+        865 => [0.1990, 74.8365],
+        896 => [0.3277, 186.2115],
+        1035 => [0.3377, 186.2115],
+        1097 => [0.3477, 186.2115],
+        1163 => [0.3527, 186.2115],
+        1232 => [0.3577, 186.2115],
+        1282 => [0.3627, 186.2115],
+        1306 => [0.3600, 182.7500],
+        1385 => [0.3650, 182.7500],
+        1468 => [0.3700, 182.7500],
+        1556 => [0.3750, 182.7500],
+        1649 => [0.3800, 182.7500],
+        1748 => [0.3850, 182.7500],
+        1853 => [0.3900, 182.7500],
+        1965 => [0.3950, 182.7500],
+        2082 => [0.4000, 182.7500],
+        2205 => [0.4050, 182.7500],
+        2307 => [0.4100, 182.7500],
+        2340 => [0.4550, 286.5962],
+        2480 => [0.4600, 286.5962],
+        2629 => [0.4650, 286.5962],
+        3461 => [0.4700, 286.5962],
+        999999999 => [0.55, 563.5192],
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isEligible(Payer $payer, Payee $payee, Earning $earning): bool
+    {
+        // Only applies after 13 October 2020.
+        if (!Date::from($earning->getPayDate(), '2020-10-13')) {
+            return false;
+        }
+
+        // Only applies to Australian residents.
+        if ($payee->getResidencyStatus() !== \ManageIt\PaygTax\Entities\Payee::RESIDENT) {
+            return false;
+        }
+
+        // Only applies to payees with a tax file number.
+        if (!$payee->hasTfnNumber()) {
+            return false;
+        }
+
+        // Only applies to payees claiming the tax free threshold.
+        if (!$payee->claimsTaxFreeThreshold()) {
+            return false;
+        }
+
+        // Only applies to payees with an STSL debt.
+        if (!$payee->hasSTSLDebt()) {
+            return false;
+        }
+
+        // Only applies to payees claiming a full Medicare Levy exemption.
+        if ($payee->getMedicareLevyExemption() !== \ManageIt\PaygTax\Entities\Payee::MEDICARE_LEVY_EXEMPTION_FULL) {
+            return false;
+        }
+
+        // Only applies to payees not claiming the Seniors Offset.
+        if ($payee->getSeniorsOffset() !== \ManageIt\PaygTax\Entities\Payee::SENIORS_OFFSET_NONE) {
+            return false;
+        }
+
+        return true;
+    }
+}
